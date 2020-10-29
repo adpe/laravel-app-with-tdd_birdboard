c824ac34f622cb9b53753917259c936a521e12ce
Adrian Perez <me@adrianperez.me>
2020-10-29 19:40:19 +0100

    Episode 5: A Project Requires An Owner

--- database/migrations/2020_10_26_201452_create_projects_table.php
+++ database/migrations/2020_10_26_201452_create_projects_table.php
@@ -15,9 +15,14 @@ class CreateProjectsTable extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            // Since Laravel 5.8 it uses bigIncrements as default.
+//            $table->unsignedBigInteger('owner_id');
+            $table->unsignedBigInteger('owner_id');
             $table->string('title');
             $table->text('description');
             $table->timestamps();
+
+            $table->foreign('owner_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+// In due of Laravel 8, we must use the class below.
+//use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @test
+     */
+    public function a_user_has_projects()
+    {
+        $user = User::factory()->create();
+
+        $this->assertInstanceOf(Collection::class, $user->projects);
+    }
+}
