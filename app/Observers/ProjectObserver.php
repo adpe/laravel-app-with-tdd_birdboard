547aa76fdb76beac890aaf50d761dc6ecff8c2d2
Adrian Perez <me@adrianperez.me>
2020-11-13 17:26:20 +0100

    Episode 21: Project Activity Feeds: Part 2

--- app/Observers/ProjectObserver.php
+++ app/Observers/ProjectObserver.php
@@ -2,7 +2,6 @@
 
 namespace App\Observers;
 
-use App\Models\Activity;
 use App\Models\Project;
 
 class ProjectObserver
@@ -10,30 +9,22 @@ class ProjectObserver
     /**
      * Handle the project "created" event.
      *
-     * @param  \App\Models\Project  $project
+     * @param  Project  $project
      * @return void
      */
     public function created(Project $project)
     {
-        $this->recordActivity('created', $project);
+        $project->recordActivity('created');
     }
 
     /**
      * Handle the project "updated" event.
      *
-     * @param  \App\Models\Project  $project
+     * @param  Project  $project
      * @return void
      */
     public function updated(Project $project)
     {
-       $this->recordActivity('updated', $project);
-    }
-
-    protected function recordActivity($type, $project)
-    {
-        Activity::create([
-            'project_id' => $project->id,
-            'description' => $type
-        ]);
+       $project->recordActivity('updated');
     }
 }
