c76d2c96e3b8c75dfd957a13e53d62f765387773
Adrian Perez <me@adrianperez.me>
2020-11-18 08:14:07 +0100

    Episode 31: Beware False Positives

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use App\Models\User;
+use Facades\Tests\Setup\ProjectFactory;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 // In due of Laravel 8, we must use the class below.
@@ -19,5 +20,26 @@ class UserTest extends TestCase
         $user = User::factory()->create();
 
         $this->assertInstanceOf(Collection::class, $user->projects);
+    }
+
+    /** @test **/
+    public function a_user_has_accessible_projects()
+    {
+        $foo = $this->signIn();
+
+        ProjectFactory::ownedBy($foo)->create();
+
+        $this->assertCount(1, $foo->accessibleProjects());
+
+        $bar = User::factory()->create();
+        $baz = User::factory()->create();
+
+        $project = tap(ProjectFactory::ownedBy($bar)->create())->invite($baz);
+
+        $this->assertCount(1, $foo->accessibleProjects());
+
+        $project->invite($foo);
+
+        $this->assertCount(2, $foo->accessibleProjects());
     }
 }
